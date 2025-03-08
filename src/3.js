aa5b5c76d5bf5635660d7a26edf76a84dbdd42d5
flukerkalaya <flukerkalaya@gmail.com>
2025-03-08 21:42:05 +0000

    Add 3.js

---
+++ src/3.js
@@ -0,0 +1,6 @@
+    function getRandomColor() {
+      var colors = ["red", "green", "blue", "orange"];
+      var index = Math.floor(Math.random() * colors.length);
+      return colors[index];
+    }
+  
