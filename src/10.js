466ff294dd0969b176ce069179793bd231d33c3f
flukerkalaya <flukerkalaya@gmail.com>
2025-03-16 10:22:37 +0000

    Add 10.js

---
+++ src/10.js
@@ -0,0 +1,24 @@
+let rand = Math.floor(Math.random() * 10 + 1);
+
+switch (rand) {
+  case 1:
+    return "Hello World!";
+  case 2:
+    return "Greetings Earthlings!";
+  case 3:
+    return "The answer to life, the universe, and everything is 42.";
+  case 4:
+    return "I am the one who knocks";
+  case 5:
+    return "In the darkness bind them, for in the darkness they will find me.";
+  case 6:
+    return "Hola! Como estan?";
+  case 7:
+    return "The meaning of life is 42. You can never have enough of it.";
+  case 8:
+    return "Bonjour! Comment allez-vous?";
+  case 9:
+    return "The answer to life, the universe, and everything is 42. Repeat after me: 42.";
+  default:
+    return "Randomly generated quote. Contact your administrator for a more informative response.";
+}
