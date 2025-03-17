7c295f97806c02b36fd00fb213c3149f4ca90f15
flukerkalaya <flukerkalaya@gmail.com>
2025-03-17 07:17:21 +0000

    Add 11.js

---
+++ src/11.js
@@ -0,0 +1,7 @@
+<script>
+// Get a random number between 1 and 10
+let randNum = Math.floor(Math.random() * 10) + 1;
+
+// Display the result on the page
+document.getElementById("randNum").innerHTML = randNum;
+</script>
