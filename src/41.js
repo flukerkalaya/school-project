db1d675b7a91f5ed749dcf983bbfb1bbeb442d77
flukerkalaya <flukerkalaya@gmail.com>
2025-05-26 20:00:25 +0000

    Add 41.js

---
+++ src/41.js
@@ -0,0 +1,6 @@
+// This is a simple school project code
+// Example: count words in a sentence
+
+const sentence = "Hello, how are you today?";
+const words = sentence.split(" ");
+console.log(words.length);
